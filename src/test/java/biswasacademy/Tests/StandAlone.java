19d02b5cdaf8260bebc9e0034fb7ae78e017f203
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-01 18:42:37 -0500

    BISWAS

--- src/test/java/biswasacademy/Tests/StandAlone.java
+++ src/test/java/biswasacademy/Tests/StandAlone.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class StandAlone {
     public static void main(String[] args) throws InterruptedException {
         String productName = "ZARA COAT 3";
-        System.out.println("1========Test Started========1");
+        System.out.println("2========Test Started========1");
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
