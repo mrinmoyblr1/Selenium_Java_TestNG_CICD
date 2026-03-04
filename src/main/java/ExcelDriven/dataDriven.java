9c859e34fc12430a0e10c1495e7ea4fc1d2f0e18
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-04 01:04:50 -0500

    BISWAS

--- src/main/java/ExcelDriven/dataDriven.java
+++ src/main/java/ExcelDriven/dataDriven.java
@@ -9,20 +9,20 @@ import java.io.IOException;
 public class dataDriven {
     public static void main(String[] args) throws IOException {
 
-        FileInputStream file = new FileInputStream("user.dir" + "/src/main/java/ExcelDriven/TestData.xlsx");
+        System.out.println(System.getProperty("user.dir"));
+
+
+        FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/ExcelDriven/TestData.xlsx");
         XSSFWorkbook workbook = new XSSFWorkbook(file);
 
         int sheets = workbook.getNumberOfSheets();
+
         for (int i = 0; i < sheets; i++) {
             if (workbook.getSheetName(i).equalsIgnoreCase("TestData")) {
                 XSSFSheet sheet = workbook.getSheetAt(i);
-                int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
-                for (int j = 0; j < rowCount + 1; j++) {
-                    String data = sheet.getRow(j).getCell(0).getStringCellValue();
-                    System.out.println(data);
 
 
-                }
+
 
             }
 
