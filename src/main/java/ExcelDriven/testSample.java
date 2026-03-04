59b0a47f78cc4bef361e76fea86c21f91ece39a6
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-04 11:23:15 -0500

    BISWAS

--- src/main/java/ExcelDriven/testSample.java
+++ src/main/java/ExcelDriven/testSample.java
@@ -1,4 +1,20 @@
 package ExcelDriven;
 
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class testSample {
+
+    @Test
+    public void testSample() throws IOException {
+        dataDriven d = new dataDriven();
+
+        ArrayList<String> data1 = d.getData("Purchase");
+
+        for (String a1 : data1) {
+            System.out.println(a1);
+        }
+    }
 }
