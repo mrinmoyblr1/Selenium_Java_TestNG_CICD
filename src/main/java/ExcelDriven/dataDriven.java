5cd859aefad797c9ea959b50b9b9d9fb7a2f4c8b
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-04 01:54:05 -0500

    BISWAS

--- src/main/java/ExcelDriven/dataDriven.java
+++ src/main/java/ExcelDriven/dataDriven.java
@@ -1,15 +1,19 @@
 package ExcelDriven;
 
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class dataDriven {
     public static void main(String[] args) throws IOException {
 
-        System.out.println(System.getProperty("user.dir"));
+        // Identify TestCases column by scanning the entire 1st row
+        // Once column is identified then scan entire TestCases column to identify purchase test case row
+        // After you grab purchase test case row then pull all the data of that row and feed into test
 
 
         FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/ExcelDriven/TestData.xlsx");
@@ -20,6 +24,14 @@ public class dataDriven {
         for (int i = 0; i < sheets; i++) {
             if (workbook.getSheetName(i).equalsIgnoreCase("TestData")) {
                 XSSFSheet sheet = workbook.getSheetAt(i);
+
+
+                // Identify TestCases column by scanning the entire 1st row
+                Iterator<Row> rows=sheet.iterator();
+                Row firstrow=rows.next();
+
+                
+
 
 
 
