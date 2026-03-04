4a0d36c5db291e2e27d1d0088b018bf9b398439f
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-04 16:02:09 -0500

    BISWAS

--- src/main/java/ExcelDriven/dataProvider.java
+++ src/main/java/ExcelDriven/dataProvider.java
@@ -6,26 +6,11 @@ import org.testng.annotations.Test;
 import java.util.Iterator;
 
 public class dataProvider {
-
-    // Multiple sets of data to our tests
-    // Array
-    // 5 sets of data - > 5 times your test will run
-
-
     @Test
     public void testCaseData() {
-
     }
-
 
     @DataProvider(name = "getData")
     public void getData() {
-        Iterator<String> it = null;
-        while (it.hasNext()) {
-            String data = it.next();
-            System.out.println(data);
-        }
     }
-
-
 }
