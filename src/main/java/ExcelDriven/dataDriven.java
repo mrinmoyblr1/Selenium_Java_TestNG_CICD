b7f111a4600203462a6955498237f224c6be3859
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-03 23:01:12 -0500

    BISWAS

--- src/main/java/ExcelDriven/dataDriven.java
+++ src/main/java/ExcelDriven/dataDriven.java
@@ -2,13 +2,16 @@ package ExcelDriven;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class dataDriven {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        XSSFWorkbook workbook = new XSSFWorkbook();
+        FileInputStream file = new FileInputStream("/Users/mrinmoy/Document-Local/Documents/Development_Local/Selenium_Java_TestNG_CICD/src/main/java/ExcelDriven");
 
 
-
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
 
 
         System.out.println("Data Driven Framework");
