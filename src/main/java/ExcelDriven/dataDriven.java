a3e55b31ea315e22879ff48b5b905616da2105a4
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-03 22:56:32 -0500

    BISWAS

---
+++ src/main/java/ExcelDriven/dataDriven.java
@@ -0,0 +1,18 @@
+package ExcelDriven;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class dataDriven {
+    public static void main(String[] args) {
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+
+
+
+
+        System.out.println("Data Driven Framework");
+
+
+    }
+}
