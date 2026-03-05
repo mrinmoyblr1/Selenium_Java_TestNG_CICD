4fdd232117d2c6c1af724c63ccdc7858668d053d
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-05 11:26:29 -0500

    BISWAS

--- src/main/java/ExcelDriven/excel.java
+++ src/main/java/ExcelDriven/excel.java
@@ -3,26 +3,16 @@ package ExcelDriven;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class dataProvider {
-
-
-    @Test(dataProvider = "getData")
-    public void testCaseData(String greeting, String communication, String id) {
-        System.out.println(greeting + communication + " " + id);
-    }
-
-
-    @DataProvider(name = "getData")
+public class excel {
+    @Test
     public void getData() throws IOException {
 //        Object[][] data = {{"Hello", " text", "1"}, {"bye", " message", "143"}, {"solo", " call", "454"}};
         // Every ROW of Excel should be one Object array
-
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/ExcelDriven/excelDriven.xlsx");
         XSSFWorkbook wb = new XSSFWorkbook(fis);
 
@@ -44,6 +34,4 @@ public class dataProvider {
             }
         }
     }
-
-
 }
