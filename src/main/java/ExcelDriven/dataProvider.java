9c1d4ee2e220911185cbf9f99ff146e2b5bc5222
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-04 19:44:38 -0500

    BISWAS

--- src/main/java/ExcelDriven/dataProvider.java
+++ src/main/java/ExcelDriven/dataProvider.java
@@ -7,13 +7,14 @@ public class dataProvider {
 
 
     @Test(dataProvider = "getData")
-    public void testCaseData() {
+    public void testCaseData(String greeting, String message, int number) {
+        System.out.println(greeting + message + " " + number);
     }
 
 
     @DataProvider(name = "getData")
     public Object[][] getData() {
-        Object[][] data = {{"Hello", " text", 1}, {"bye", " message", 143}, {"solo", " call", 453}};
+        Object[][] data = {{"Hello", " text", 1}, {"bye", " message", 143}, {"solo", " call", 454}};
         return data;
     }
 }
