450615600fcf052b1b91b3d12e4328732447f262
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-04 21:46:27 -0500

    BISWAS

--- src/main/java/ExcelDriven/dataProvider.java
+++ src/main/java/ExcelDriven/dataProvider.java
@@ -1,11 +1,12 @@
 package ExcelDriven;
 
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class dataProvider {
@@ -25,6 +26,13 @@ public class dataProvider {
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/ExcelDriven/excelDriven.xlsx");
         XSSFWorkbook wb = new XSSFWorkbook(fis);
 
+        XSSFSheet sheet = wb.getSheetAt(0);
+        int rowCount = sheet.getPhysicalNumberOfRows();
+
+        XSSFRow row = sheet.getRow(0);
+        int column_Count = row.getLastCellNum();
+
+        Object[][] data = new Object[rowCount - 1][column_Count];
 
 
     }
