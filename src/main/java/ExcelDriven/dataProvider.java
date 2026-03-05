6f0089f4a56c3d5b1f39bcc3ace924147a434ba9
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-05 16:33:42 -0500

    BISWAS

--- src/main/java/ExcelDriven/dataProvider.java
+++ src/main/java/ExcelDriven/dataProvider.java
@@ -12,8 +12,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class dataProvider {
-
-
     DataFormatter formatter = new DataFormatter();
 
     @Test(dataProvider = "getData")
@@ -21,28 +19,20 @@ public class dataProvider {
         System.out.println(greeting + communication + " " + id);
     }
 
-
     @DataProvider(name = "getData")
     public Object[][] getData() throws IOException {
 //        Object[][] data = {{"Hello", " text", "1"}, {"bye", " message", "143"}, {"solo", " call", "454"}};
         // Every ROW of Excel should be one Object array
-
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/ExcelDriven/excelDriven.xlsx");
         XSSFWorkbook wb = new XSSFWorkbook(fis);
-
         XSSFSheet sheet = wb.getSheetAt(0);
         int rowCount = sheet.getPhysicalNumberOfRows();
-
         XSSFRow row = sheet.getRow(0); // It will return the first row of the sheet and we can get the column count from that row
         int column_Count = row.getLastCellNum();
-
         Object[][] data = new Object[rowCount - 1][column_Count];
-
         for (int i = 0; i < rowCount - 1; i++) {
             row = sheet.getRow(i + 1);
-
             for (int j = 0; j < column_Count; j++) {
-
                 XSSFCell cell = row.getCell(j);
                 data[i][j] = formatter.formatCellValue(cell);
             }
