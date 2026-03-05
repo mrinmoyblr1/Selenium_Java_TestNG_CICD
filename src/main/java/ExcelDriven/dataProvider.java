32ed3f5d3e95806506dfd41b66a07fe682a52bc5
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-04 21:04:35 -0500

    BISWAS

--- src/main/java/ExcelDriven/dataProvider.java
+++ src/main/java/ExcelDriven/dataProvider.java
@@ -1,20 +1,36 @@
 package ExcelDriven;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class dataProvider {
 
 
     @Test(dataProvider = "getData")
-    public void testCaseData(String greeting, String communication, int id) {
+    public void testCaseData(String greeting, String communication, String id) {
         System.out.println(greeting + communication + " " + id);
     }
 
 
     @DataProvider(name = "getData")
-    public Object[][] getData() {
-        Object[][] data = {{"Hello", " text", 1}, {"bye", " message", 143}, {"solo", " call", 454}};
+    public Object[][] getData() throws IOException {
+        Object[][] data = {{"Hello", " text", "1"}, {"bye", " message", "143"}, {"solo", " call", "454"}};
+        // Every ROW of Excel should be one Object array
+
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/ExcelDriven/excelDriven.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+
+        int sheets = workbook.getNumberOfSheets();
+        for (int i = 0; i < sheets; i++) {
+
+        }
+
+
         return data;
     }
 }
