6bea648301db3a13960507b2a0bd1fcfbc40ee9b
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-06 16:51:37 -0500

    BISWAS

--- src/main/java/UploadDownload/UploadDownlaod.java
+++ src/main/java/UploadDownload/UploadDownlaod.java
@@ -24,20 +24,20 @@ public class UploadDownlaod {
         driver.findElement(By.cssSelector("#downloadButton")).click();
         // Edit Excel
 
+
         WebElement upload = driver.findElement(By.cssSelector("input[type='file']"));
         upload.sendKeys(System.getProperty("user.dir") + "/src/main/java/UploadDownload/download.xlsx");
 
 
         // Wait for Success Message to show up ad wait for disappearing
-        // Below is the Implicit wait
-
-        By toastLocator = By.cssSelector(".Toastify__toast-body dev:nth-child(2)");
+        By toastLocator = By.cssSelector(".Toastify__toast-body div:nth-child(2");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(toastLocator));
-
-
+        wait.until(ExpectedConditions.visibilityOfElementLocated(toastLocator));
         String toastText = driver.findElement(toastLocator).getText();
+        System.out.println(toastText);
         Assert.assertEquals(toastText, "Updated Excel Data Successfully.");
+
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(toastLocator));
 
 
         // Verify updated Excel data showing in the Web page
