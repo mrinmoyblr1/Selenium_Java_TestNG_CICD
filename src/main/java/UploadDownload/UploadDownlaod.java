9d67502c875725153bd54911734148604fa79ba2
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-06 15:59:14 -0500

    BISWAS

--- src/main/java/UploadDownload/UploadDownlaod.java
+++ src/main/java/UploadDownload/UploadDownlaod.java
@@ -16,16 +16,25 @@ public class UploadDownlaod {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         driver.get("https://rahulshettyacademy.com/upload-download-test/");
 
-        driver.findElement(By.cssSelector("#fileinput")).click();
-        WebElement upload = driver.findElement(By.cssSelector("#fileinput"));
-        upload.sendKeys("/Users/mrinmoy/Downloads/test.txt");
+        driver.findElement(By.cssSelector("#downloadButton")).click();
+        // Edit Excel
+
+        WebElement upload = driver.findElement(By.cssSelector("input[type='file']"));
+        upload.sendKeys(System.getProperty("user.dir") + "/src/main/java/UploadDownload/download.xlsx");
 
 
-        System.out.println("Upload and Download");
-        Thread.sleep(4000);
+        // Wait for Success Message to show up ad wait for disappearing
+
+
+
+        // Verify updated Excel data showing in the Web page
+
+
+
+
+        Thread.sleep(10000);
         driver.quit();
     }
 }
