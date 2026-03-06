b53c30d9ac8fd6fb7439d4b557fec9513bfe48b0
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-06 16:16:54 -0500

    BISWAS

--- src/main/java/UploadDownload/UploadDownlaod.java
+++ src/main/java/UploadDownload/UploadDownlaod.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -26,12 +29,18 @@ public class UploadDownlaod {
 
 
         // Wait for Success Message to show up ad wait for disappearing
+        // Below is the Implicit wait
 
+        By toastLocator = By.cssSelector(".Toastify__toast-body dev:nth-child(2)");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(toastLocator));
+
+
+        String toastText = driver.findElement(toastLocator).getText();
+        Assert.assertEquals(toastText, "Updated Excel Data Successfully.");
 
 
         // Verify updated Excel data showing in the Web page
-
-
 
 
         Thread.sleep(10000);
