7e7af9558baa5cfe177be40694c9ed8c6a5f9fa2
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-06 10:05:48 -0500

    BISWAS

--- src/main/java/ExcelDriven/dataProvider.java
+++ src/main/java/ExcelDriven/dataProvider.java
@@ -34,8 +34,11 @@ public class dataProvider {
         XSSFRow row = sheet.getRow(0);
         int column_Count = row.getLastCellNum();
 
-        
-
+        // Create a 2D array to store the data from the sheet
+        // We are using rowCount - 1 because we are skipping the header row
+        // We are using column_Count because we want to store all the columns in the sheet
+        // We are using Object[][] because we want to store different types of data in the sheet (String, int, etc.)
+        // We are using Object[][] because we want to return the data from the method and use it in the test method
         Object[][] data = new Object[rowCount - 1][column_Count];
         for (int i = 0; i < rowCount - 1; i++) {
             row = sheet.getRow(i + 1);
