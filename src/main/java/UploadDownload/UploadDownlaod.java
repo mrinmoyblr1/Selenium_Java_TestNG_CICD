efea808f2b81c5a74826f19eb0a9bb50caebb8d3
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-06 13:01:09 -0500

    BISWAS

--- src/main/java/UploadDownload/UploadDownlaod.java
+++ src/main/java/UploadDownload/UploadDownlaod.java
@@ -1,19 +1,26 @@
 package UploadDownload;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class UploadDownlaod {
     @Test
-    public void uploadDownload() {
+    public void uploadDownload() throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rahulshettyacademy.com/upload-download-test/");
+
+        driver.findElement(By.id("downloadButton")).click();
 
 
         System.out.println("Upload and Download");
+        Thread.sleep(4000);
         driver.quit();
     }
 }
