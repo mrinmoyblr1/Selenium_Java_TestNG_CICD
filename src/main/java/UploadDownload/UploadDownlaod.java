f9ee2b10db6448580ef51cb26927283d865c73db
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-06 13:54:48 -0500

    BISWAS

--- src/main/java/UploadDownload/UploadDownlaod.java
+++ src/main/java/UploadDownload/UploadDownlaod.java
@@ -2,21 +2,26 @@ package UploadDownload;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class UploadDownlaod {
+
     @Test
     public void uploadDownload() throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         driver.get("https://rahulshettyacademy.com/upload-download-test/");
 
-        driver.findElement(By.id("downloadButton")).click();
+        driver.findElement(By.cssSelector("#fileinput")).click();
+        WebElement upload = driver.findElement(By.cssSelector("#fileinput"));
+        upload.sendKeys("/Users/mrinmoy/Downloads/test.txt");
 
 
         System.out.println("Upload and Download");
