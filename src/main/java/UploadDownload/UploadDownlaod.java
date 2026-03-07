de54369dce2920ec8c34ff2d7f490cb5aacb3f2a
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-06 22:42:18 -0500

    BISWAS

--- src/main/java/UploadDownload/UploadDownlaod.java
+++ src/main/java/UploadDownload/UploadDownlaod.java
@@ -46,12 +46,14 @@ public class UploadDownlaod {
         String priceColumn = driver.findElement(By.xpath("//div[text()='Price']")).getAttribute("data-column-id");
         System.out.println(priceColumn);
         String actualPrice = driver.findElement(By.xpath("//div[text()='" + fruitName + "']/parent::div/parent::div/div[@id='cell-" + priceColumn + "-undefined']")).getText();
+        // Below are two examples of xPath of above
+        ////div[text()='Apple']/parent::div/following-sibling::div[2]
+        ////div[text()='Apple']/parent::div/parent::div/div[@id='cell-4-undefined']
         System.out.println("The Actual Fruit Price: "+actualPrice);
         Assert.assertEquals(actualPrice, "999");
 
 
-        ////div[text()='Apple']/parent::div/following-sibling::div[2]
-        ////div[text()='Apple']/parent::div/parent::div/div[@id='cell-4-undefined']
+
 
 
         Thread.sleep(2000);
