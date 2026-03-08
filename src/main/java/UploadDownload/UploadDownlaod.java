a87808796f63e294186bc34cb043a900475dcc81
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-08 08:48:01 -0400

    BISWAS

--- src/main/java/UploadDownload/UploadDownlaod.java
+++ src/main/java/UploadDownload/UploadDownlaod.java
@@ -1,5 +1,9 @@
 package UploadDownload;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,7 +13,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class UploadDownlaod {
 
@@ -27,9 +36,9 @@ public class UploadDownlaod {
         // Edit Excel - getColumnNumber of Price -getRowNUmber of Apple -> Update Excel Wow, Col
 
 
-        // inr col=getCOlNumber("Price")
-        // int row=getRowNumber("Apple")
-        // updateCell()
+        int col = getColNumber(fileName, "Price");
+        int row = getRowNumber(fileName, "Apple");
+        updateCell(fileName, row, cal, "599");
 
 
         WebElement upload = driver.findElement(By.cssSelector("input[type='file']"));
@@ -61,4 +70,49 @@ public class UploadDownlaod {
         Thread.sleep(2000);
         driver.quit();
     }
-}
+
+
+    public int getColNumber(String fileName, String columnName) throws IOException {
+
+
+        ArrayList<String> a = new ArrayList<String>();
+        FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/ExcelDriven/TestData.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        int sheets = workbook.getNumberOfSheets();
+
+        for (int i = 0; i < sheets; i++) {
+            if (workbook.getSheetName(i).equalsIgnoreCase("TestData")) {
+                XSSFSheet sheet = workbook.getSheetAt(i);
+
+                // Identify the TestCases column by scanning the entire 1st row
+                Iterator<Row> rows = sheet.iterator();  // sheet is a collection of rows
+                // To get the first row
+                Row firstrow = rows.next();
+
+
+                // To get the column number of TestCases
+                Iterator<Cell> ce = firstrow.cellIterator(); // row is a collection of cells
+                int k = 0;
+                int column = 0;
+                while (ce.hasNext()) {
+                    Cell value = ce.next();
+                    if (value.getStringCellValue().equalsIgnoreCase("TestCases")) {
+                        column = k;
+                    }
+                    k++;
+                }
+
+
+                return 0;
+            }
+
+
+            public int getRowNumber (String fileName, String fruitName){
+                return 0;
+            }
+
+
+            public void updateCell (String fileName,int row, int col, String value){
+
+            }
+        }
